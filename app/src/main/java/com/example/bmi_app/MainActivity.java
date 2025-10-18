2e53b78d6d94abb3694188d6c57e6c82284850ef
Pranay1012904 <pranay.dmnsns@gmail.com>
2025-10-18 19:16:44 +0530

    Added Toast

--- app/src/main/java/com/example/bmi_app/MainActivity.java
+++ app/src/main/java/com/example/bmi_app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.bmi_app;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +14,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+       // EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);//the XML file
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+        Toast.makeText(this, "This is a BMI CALCULATOR APP!", Toast.LENGTH_LONG).show();
     }
 }
